866696275977a6b0680295466d3bb20c4b1b5c1f
Polina Andreyanova <andreyanovalina@yandex.ru>
2023-01-04 20:51:24 +0300

    feat: добавить краш-тест и ссылки на frontend и backend

--- backend/app.js
+++ backend/app.js
@@ -54,6 +54,12 @@ app.use('/users', checkAuth, userRouter);
 
 app.use('/cards', checkAuth, cardRouter);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
