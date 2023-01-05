3c94021addb3f6c923da58db4b608aed06f8b50e
Polina Andreyanova <andreyanovalina@yandex.ru>
2023-01-05 13:49:09 +0300

    feat: исправить ошибки, связанные с CORS

--- backend/app.js
+++ backend/app.js
@@ -25,6 +25,7 @@ app.use(bodyParser.json());
 const allowedCors = [
   'http://localhost:3000',
   'http://mesto.polina.students.nomoredomains.club',
+  'https://mesto.polina.students.nomoredomains.club',
 ];
 
 app.use((req, res, next) => {
