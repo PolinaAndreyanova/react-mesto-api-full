df3722af16be26527f730918b78a31c6cc75ec32
Polina Andreyanova <andreyanovalina@yandex.ru>
2023-01-05 13:46:33 +0300

    feat: обновить сборку frontend

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://api.mesto.polina.students.nomoredomains.club";
+const baseUrl = "https://api.mesto.polina.students.nomoredomains.club";
 
 const checkResponse = (res) => res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
 
